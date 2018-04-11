a34d9a29cd9ad9af51daa413a7e3f6015867745b
amy <anmingyu@smartisan.com>
2018-04-11 16:48:04 +0800

    Finished this.
    Signed-off-by: amy <anmingyu@smartisan.com>

--- Chap1/UidGid.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-
-int main()
-{
-    printf("uid = %d , gid = %d\n", getuid(), getgid());
-}
--- Chap1/getPID.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-
-int main(void)
-{
-    printf("pid : %ld", (long)getpid());
-    exit(0);
-}
